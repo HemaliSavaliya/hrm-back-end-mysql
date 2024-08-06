597ca1df7762e720e2e6a192816f7cc171512286
hemali <hemalisavaliya554@gmail.com>
2024-08-06 14:31:19 +0530

    config file change

--- config/config.js
+++ config/config.js
@@ -1,64 +1,86 @@
-// const mysql = require('mysql');
+// const mysql = require("mysql");
 // const dotenv = require("dotenv");
 // dotenv.config();
 
 // // Database connection configuration
 // const connection = mysql.createConnection({
-//     connectionLimit : 100,
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'hrm'
+//   connectionLimit: 100,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "hrm",
 // });
 
 // // Connect to MySQL database
 // connection.connect((err) => {
-//     if (err) {
-//         console.error('Error connecting to database: ' + err.stack);
-//         return;
-//     }
-//     console.log('Connected to database');
+//   if (err) {
+//     console.error("Error connecting to database: " + err.stack);
+//     return;
+//   }
+//   console.log("Connected to database");
 // });
 
 // module.exports = connection;
 
+// // For live server
+// const mysql = require("mysql");
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-// For live server
+// const dbConfig = {
+//     host: process.env.DATABASE_HOST,
+//     user: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASSWORD,
+//     database: process.env.DATABASE_NAME
+// };
+
+// let connection;
+
+// function handleDisconnect() {
+//     connection = mysql.createConnection(dbConfig);
+
+//     connection.connect((err) => {
+//         if (err) {
+//             console.error("Error connecting to database: ", err.stack);
+//             setTimeout(handleDisconnect, 2000); // Reconnect after 2 seconds
+//         } else {
+//             console.log("Connected to database");
+//         }
+//     });
+
+//     connection.on('error', (err) => {
+//         console.error("Database error", err);
+//         if (err.code === "PROTOCOL_CONNECTION_LOST") {
+//             handleDisconnect();
+//         } else {
+//             throw err;
+//         }
+//     });
+// }
+
+// handleDisconnect();
+
+// module.exports = connection;
+
+// For live with the use of pool
 const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const dbConfig = {
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
-};
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
 
-let connection;
+pool.on("connection", (connection) => {
+  console.log("Connected to database");
+});
 
-function handleDisconnect() {
-    connection = mysql.createConnection(dbConfig);
+pool.on("error", (err) => {
+  console.error("Database error", err);
+});
 
-    connection.connect((err) => {
-        if (err) {
-            console.error("Error connecting to database: ", err.stack);
-            setTimeout(handleDisconnect, 2000); // Reconnect after 2 seconds
-        } else {
-            console.log("Connected to database");
-        }
-    });
-
-    connection.on('error', (err) => {
-        console.error("Database error", err);
-        if (err.code === "PROTOCOL_CONNECTION_LOST") {
-            handleDisconnect();
-        } else {
-            throw err;
-        }
-    });
-}
-
-handleDisconnect();
-
-module.exports = connection;
+module.exports = pool;
--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,67 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const connection = require("../config/config");
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+// const connection = require("../config/config");
+
+const pool = require("../config/config");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+module.exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const sql = `
+            SELECT id, name, email, password, NULL AS deleted, NULL AS companyId, 'SuperAdmin' AS role, 'superadmin' AS user_type FROM hrm_superadmin WHERE email = ?
+            UNION ALL
+            SELECT id, name, email, password, deleted, companyId, 'Admin' AS role, 'admin' AS user_type FROM hrm_admins WHERE email = ?
+            UNION ALL
+            SELECT id, name, email, password, deleted, companyId, role, 'employee' AS user_type FROM hrm_employees WHERE email = ?
+        `;
+
+    pool.query(sql, [email, email, email], async (err, results) => {
+      if (err) {
+        console.error("Error during login:", err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+
+      if (results.length === 0) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      let user = results[0]; // Assuming the user is unique across tables
+
+      if (user.deleted) {
+        return res.status(401).json({ error: "User Account is deleted" });
+      }
+
+      // Check if the password is correct
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return res.status(401).send({ error: "Invalid password" });
+      }
+
+      // Authenticate successfully, generate and return a token
+      const token = jwt.sign(
+        { id: user.id, role: user.role },
+        process.env.JWT_SECRET_KEY
+      );
+
+      res.status(200).json({
+        data: {
+          token,
+          companyId: user.companyId,
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        },
+      });
+    });
+  } catch (error) {
+    console.error("Error during login", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 // module.exports.login = async (req, res) => {
 //     try {
@@ -76,93 +137,102 @@ const connection = require("../config/config");
 //     }
 // }
 
-module.exports.login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+// module.exports.login = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
 
-        // Query all tables to find user by email
-        const sql = `
-            SELECT id, name, email, password, NULL AS deleted, NULL AS companyId, 'SuperAdmin' AS role, 'superadmin' AS user_type FROM hrm_superadmin WHERE email = ?
-            UNION ALL
-            SELECT id, name, email, password, deleted, companyId, 'Admin' AS role, 'admin' AS user_type FROM hrm_admins WHERE email = ?
-            UNION ALL
-            SELECT id, name, email, password, deleted, companyId, role, 'employee' AS user_type FROM hrm_employees WHERE email = ?        
-        `;
+//     // Query all tables to find user by email
+//     const sql = `
+//             SELECT id, name, email, password, NULL AS deleted, NULL AS companyId, 'SuperAdmin' AS role, 'superadmin' AS user_type FROM hrm_superadmin WHERE email = ?
+//             UNION ALL
+//             SELECT id, name, email, password, deleted, companyId, 'Admin' AS role, 'admin' AS user_type FROM hrm_admins WHERE email = ?
+//             UNION ALL
+//             SELECT id, name, email, password, deleted, companyId, role, 'employee' AS user_type FROM hrm_employees WHERE email = ?
+//         `;
 
-        connection.query(sql, [email, email, email], async (err, results) => {
-            if (err) {
-                console.error("Error during login:", err);
-                return res.status(500).json({ error: "Internal Server Error" });
-            }
+//     connection.query(sql, [email, email, email], async (err, results) => {
+//       if (err) {
+//         console.error("Error during login:", err);
+//         return res.status(500).json({ error: "Internal Server Error" });
+//       }
 
-            if (results.length === 0) {
-                return res.status(404).json({ error: "User not found" });
-            }
+//       if (results.length === 0) {
+//         return res.status(404).json({ error: "User not found" });
+//       }
 
-            let user = null;
-            if (results.length > 0) {
-                user = results[0]; // Assuming the user is unique across tables
-            }
+//       let user = null;
+//       if (results.length > 0) {
+//         user = results[0]; // Assuming the user is unique across tables
+//       }
 
-            if (user.deleted) {
-                return res.status(401).json({ error: "User Account is deleted" });
-            }
+//       if (user.deleted) {
+//         return res.status(401).json({ error: "User Account is deleted" });
+//       }
 
-            // Check if the password is correct
-            const passwordMatch = await bcrypt.compare(password, user.password);
-            if (!passwordMatch) {
-                return res.status(401).send({ error: "Invalid password" });
-            }
+//       // Check if the password is correct
+//       const passwordMatch = await bcrypt.compare(password, user.password);
+//       if (!passwordMatch) {
+//         return res.status(401).send({ error: "Invalid password" });
+//       }
 
-            // Authenticate successfully, generate and return a token
-            const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET_KEY);
+//       // Authenticate successfully, generate and return a token
+//       const token = jwt.sign(
+//         { id: user.id, role: user.role },
+//         process.env.JWT_SECRET_KEY
+//       );
 
-            res.status(200).json({
-                data: {
-                    token,
-                    companyId: user.companyId,
-                    id: user.id,
-                    name: user.name,
-                    email: user.email,
-                    role: user.role
-                }
-            });
-        });
-    } catch (error) {
-        console.error("Error during login", error);
-        return res.status(500).json({ error: "Internal Server Error" });
-    }
-}
+//       res.status(200).json({
+//         data: {
+//           token,
+//           companyId: user.companyId,
+//           id: user.id,
+//           name: user.name,
+//           email: user.email,
+//           role: user.role,
+//         },
+//       });
+//     });
+//   } catch (error) {
+//     console.error("Error during login", error);
+//     return res.status(500).json({ error: "Internal Server Error" });
+//   }
+// };
 
 module.exports.logout = async (req, res) => {
-    try {
-        res.status(200).json({ message: "Logout successfully" });
-    } catch (error) {
-        console.error("Error during logout", error);
-        return res.status(500).json({ error: "Internal Server Error" });
-    }
-}
+  try {
+    res.status(200).json({ message: "Logout successfully" });
+  } catch (error) {
+    console.error("Error during logout", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 module.exports.addSuperAdmin = async (req, res) => {
-    try {
-        const { name, email, password, role } = req.body;
+  try {
+    const { name, email, password, role } = req.body;
 
-        // Insert the new user into the superadmin table
-        const insertSql = `INSERT INTO hrm_superadmin (name, email, password, role) VALUES (?, ?, ?, ?)`;
+    // Insert the new user into the superadmin table
+    const insertSql = `INSERT INTO hrm_superadmin (name, email, password, role) VALUES (?, ?, ?, ?)`;
 
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        connection.query(insertSql, [name, email, hashedPassword, role || "SuperAdmin"], (err, result) => {
-            if (err) {
-                console.error("Error adding SuperAdmin:", err);
-                return res.status(500).json({ error: "Internal Server Error" });
-            }
+    connection.query(
+      insertSql,
+      [name, email, hashedPassword, role || "SuperAdmin"],
+      (err, result) => {
+        if (err) {
+          console.error("Error adding SuperAdmin:", err);
+          return res.status(500).json({ error: "Internal Server Error" });
+        }
 
-            res.status(201).json({ name, email, password, role: role || "SuperAdmin" });
-        });
-    } catch (error) {
-        console.error("Error adding SuperAdmin", error);
-        return res.status(500).json({ error: "Internal Server Error" });
-    }
-}
+        res
+          .status(201)
+          .json({ name, email, password, role: role || "SuperAdmin" });
+      }
+    );
+  } catch (error) {
+    console.error("Error adding SuperAdmin", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
