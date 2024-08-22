a56ae73a33d487b775880e93143ab701c42e8856
hemali <hemalisavaliya554@gmail.com>
2024-08-22 10:12:26 +0530

    Change config file

--- config/config.js
+++ config/config.js
@@ -1,27 +1,3 @@
-// const mysql = require("mysql");
-// const dotenv = require("dotenv");
-// dotenv.config();
-
-// // Database connection configuration
-// const connection = mysql.createConnection({
-//   connectionLimit: 100,
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "hrm",
-// });
-
-// // Connect to MySQL database
-// connection.connect((err) => {
-//   if (err) {
-//     console.error("Error connecting to database: " + err.stack);
-//     return;
-//   }
-//   console.log("Connected to database");
-// });
-
-// module.exports = connection;
-
 // For live with the use of pool
 const mysql = require("mysql");
 const dotenv = require("dotenv");
