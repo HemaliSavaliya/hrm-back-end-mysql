b751e5e6a6c4ec3c4fb560a1e7f8c629d875d95f
hemali <hemalisavaliya554@gmail.com>
2024-09-10 15:28:31 +0530

    deleted=true add in company

--- controller/companyController.js
+++ controller/companyController.js
@@ -373,28 +373,6 @@ module.exports.deleteCompany = async (req, res) => {
   });
 };
 
-// module.exports.companyList = async (req, res) => {
-//   try {
-//     const sql = "SELECT * FROM hrm_companys";
-
-//     pool.query(sql, (err, result) => {
-//       if (err) {
-//         console.error("Error Fetching Company", err);
-//         return res.status(500).json({ error: "Internal Server Error" });
-//       }
-
-//       if (result.length > 0) {
-//         res.status(200).json(result);
-//       } else {
-//         return res.status(404).json({ error: "No Company Found!" });
-//       }
-//     });
-//   } catch (error) {
-//     console.error("Error Fetching Company List", error);
-//     return res.status(500).json({ error: "Internal Server Error" });
-//   }
-// };
-
 module.exports.companyList = async (req, res) => {
   try {
     const page = Number(req.query.page) || 1;
@@ -421,7 +399,7 @@ module.exports.companyList = async (req, res) => {
     }
 
     // count total items with filtering
-    const countQuery = `SELECT COUNT(*) AS count FROM hrm_companys WHERE companyName LIKE ?`;
+    const countQuery = `SELECT COUNT(*) AS count FROM hrm_companys WHERE deleted=true AND companyName LIKE ?`;
 
     pool.query(countQuery, [`%${search}%`], (err, countResult) => {
       if (err) {
